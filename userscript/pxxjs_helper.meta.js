149ef9b5e45ee4d6c575a9d5e2e9c51d18094fe9
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-24 14:54:53 +0800

    Update pxxjs_helper.meta.js

--- userscript/pxxjs_helper.meta.js
+++ userscript/pxxjs_helper.meta.js
@@ -1,7 +1,7 @@
 ---
 ---
 // ==UserScript==
-// @version      {{ site.version }}
+// @version      {{ pxx917144686 }}
 // @name         pxxjs helper
 // @namespace    https://github.com/pxx917144686
 // ==/UserScript==
